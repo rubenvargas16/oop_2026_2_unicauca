393bb7e7c80926f410911bf3c4138ae651a6dd9c
ruben4181 <ruben4181@gmail.com>
2026-09-01 23:39:36 -0500

    fix: sesion 4 init

--- sesion_3/libros.cpp
+++ sesion_3/libros.cpp
@@ -2,126 +2,141 @@
 #include <string>
 using namespace std;
 
-class Autor{
-  private:
-    string nombre;
-    string nacionalidad;
-    string fechaNacimiento;
-    string fechaMuerte;
-    int librosPublicados;
-  public:
-    Autor(){
-      this->nombre="";
-      this->nacionalidad="";
-      this->fechaNacimiento="";
-      this->fechaMuerte="";
-      this->librosPublicados=1;
-    }
+class Autor
+{
+private:
+  string nombre;
+  string nacionalidad;
+  string fechaNacimiento;
+  string fechaMuerte;
+  int librosPublicados;
 
-    Autor(string nombre, string nacionalidad, string fechaNacimiento, string fechaMuerte, int librosPublicados){
-      this->nombre=nombre;
-      this->nacionalidad=nacionalidad;
-      this->fechaNacimiento=fechaNacimiento;
-      this->fechaMuerte=fechaMuerte;
-      this->librosPublicados=librosPublicados;
-    }
-    //Setters
-    void setNombre(string nombre){ this->nombre = nombre; }
-    void setNacionalidad(string nacionalidad) { this->nacionalidad = nacionalidad; }
-    void setFechaNacimiento(string fechaNacimiento) { this->fechaNacimiento = fechaNacimiento; }
-    void setFechaMuerte(string fechaMuerte) { this->fechaMuerte = fechaMuerte; }
-    void setLibrosPublicados(int librosPublicados) { this->librosPublicados; }
-    //Getters
-    string getNombre() { return this->nombre; }
-    string getNacionalidad() { return this->nacionalidad; }
-    string getFechaNacimiento(){ return this->fechaNacimiento; }
-    string getFechaMuerte(){ return this->fechaMuerte; }
-    int getLibrosPublicados(){ return this->librosPublicados; }
+public:
+  Autor()
+  {
+    this->nombre = "";
+    this->nacionalidad = "";
+    this->fechaNacimiento = "";
+    this->fechaMuerte = "";
+    this->librosPublicados = 1;
+  }
 
-    void print(){
-      cout << "Informacion del autor" << endl;
-      cout << "Nombre: " << this->nombre << endl;
-      cout << "Nacionalidad: " << this->nacionalidad << endl;
-      cout << "Fecha de nacimiento: " << this->fechaNacimiento << endl;
-      if(this->fechaMuerte.length() > 0){
-        cout << "Fecha de muerte: " << this->fechaMuerte << endl;
-      }
-      cout << "Libros publicados: " << this->librosPublicados << endl;
+  Autor(string nombre, string nacionalidad, string fechaNacimiento, string fechaMuerte, int librosPublicados)
+  {
+    this->nombre = nombre;
+    this->nacionalidad = nacionalidad;
+    this->fechaNacimiento = fechaNacimiento;
+    this->fechaMuerte = fechaMuerte;
+    this->librosPublicados = librosPublicados;
+  }
+  // Setters
+  void setNombre(string nombre) { this->nombre = nombre; }
+  void setNacionalidad(string nacionalidad) { this->nacionalidad = nacionalidad; }
+  void setFechaNacimiento(string fechaNacimiento) { this->fechaNacimiento = fechaNacimiento; }
+  void setFechaMuerte(string fechaMuerte) { this->fechaMuerte = fechaMuerte; }
+  void setLibrosPublicados(int librosPublicados) { this->librosPublicados = librosPublicados; }
+  // Getters
+  string getNombre() { return this->nombre; }
+  string getNacionalidad() { return this->nacionalidad; }
+  string getFechaNacimiento() { return this->fechaNacimiento; }
+  string getFechaMuerte() { return this->fechaMuerte; }
+  int getLibrosPublicados() { return this->librosPublicados; }
+
+  void print()
+  {
+    cout << "Informacion del autor" << endl;
+    cout << "Nombre: " << this->nombre << endl;
+    cout << "Nacionalidad: " << this->nacionalidad << endl;
+    cout << "Fecha de nacimiento: " << this->fechaNacimiento << endl;
+    if (this->fechaMuerte.length() > 0)
+    {
+      cout << "Fecha de muerte: " << this->fechaMuerte << endl;
     }
-    
+    cout << "Libros publicados: " << this->librosPublicados << endl;
+  }
 };
 
-class Libro{
-  private:
-    string title;
-    Autor author;
-    int releaseYear;
-    string isbn;
-    int stock; //Cantidad
-  public:
-    // Constructor por defecto ó vacío.
-    Libro(){
-      this->title = "No title";
+class Libro
+{
+private:
+  string title;
+  Autor author;
+  int releaseYear;
+  string isbn;
+  int stock; // Cantidad
+public:
+  // Constructor por defecto ó vacío.
+  Libro()
+  {
+    this->title = "No title";
+    this->releaseYear = 0;
+    this->isbn = "NO-ISBN";
+    this->stock = 0;
+  }
+  // Constructor
+  Libro(string title, Autor author, int stock)
+  {
+    this->title = title;
+    this->author = author;
+    this->stock = stock;
+  }
+  // Sobrecarga de constructor
+  Libro(string title, Autor author) : Libro(title, author, 1)
+  {
+    this->releaseYear = 2026;
+    this->isbn = "NO-ISBN";
+  }
+
+  string getTitle() const { return this->title; }
+  Autor getAuthor() const { return this->author; }
+  int getReleaseYear() const { return this->releaseYear; }
+  string getISBN() const { return this->isbn; }
+  int getStock() const { return this->stock; }
+
+  void setTitle(string title) { this->title = title; }
+  void setAuthor(Autor author) { this->author = author; }
+  void setReleaseYear(int releaseYear) { this->releaseYear = releaseYear; }
+  void setISBN(string isbn)
+  {
+    this->isbn = isbn;
+  }
+  void setStock(int stock)
+  {
+    if (stock < 0)
+    {
       this->stock = 0;
+      cout << "El stock no puede ser negativo" << endl;
     }
-    //Constructor
-    Libro(string title, Autor author, int stock) {
-      this->title = title;
+    else
+    {
       this->stock = stock;
     }
-    //Sobrecarga de constructor
-    Libro(string title, Autor author) : Libro(title, author, 1){
-      this->releaseYear = 2026;
-      this->isbn = "NO-ISBN";
-    }
+  }
 
-
-    string getTitle() const { return this->title; }
-    Autor getAuthor() const { return this->author; }
-    int getReleaseYear() const { return this->releaseYear; }
-    string getISBN() const { return this->isbn; }
-    int getStock() const { return this->stock; }
-
-    void setTitle(string title){ this->title = title; }
-    void setAuthor(Autor author){ this->author = author; }
-    void setReleaseYear(int releaseYear){ this->releaseYear = releaseYear; }
-    void setISBN(string isbn) { 
-      this->isbn = isbn;
-    }
-    void setStock(int stock){
-      if(stock < 0){
-        this->stock = 0;
-        cout << "El stock no puede ser negativo" << endl;
-      } else{
-        this->stock = stock;
-      }
-    }
-
-    void print(){
-      cout << "Información del libro:" << endl;
-      cout << "Titulo: " << this->title << endl;
-      this->author.print();
-      cout << "Año de publicación: " << this->releaseYear << endl;
-      cout << "ISBN: " << this->isbn << endl;
-      cout << "Stock: " << this ->stock << endl;
-    }
+  void print()
+  {
+    cout << "Información del libro:" << endl;
+    cout << "Titulo: " << this->title << endl;
+    this->author.print();
+    cout << "Año de publicación: " << this->releaseYear << endl;
+    cout << "ISBN: " << this->isbn << endl;
+    cout << "Stock: " << this->stock << endl;
+  }
 };
 
-
-
-int main(){
+int main()
+{
   Libro myBook;
-  myBook.print();
-  //Libro myBook2("La Biblia", "LG-sus", 100);
-  //Libro myBook3("2001", "SDASD");
 
   myBook.setTitle("El jugador");
-  //myBook.setAuthor("Fiodor Dostoievsky");
+  // myBook.setAuthor("Fiodor Dostoievsky");
   myBook.setReleaseYear(1861);
   myBook.setISBN("ISBN_FAKE");
   myBook.setStock(20);
-  
-  //Imprimir la data del libro
-  //myBook.print();
+
+  Autor autor("Fiodor Dostoievsky", "Rusia", "1700-01-01", "1763-01-01", 10);
+  myBook.setAuthor(autor);
+  // Imprimir la data del libro
+  myBook.print();
   return 0;
 }
